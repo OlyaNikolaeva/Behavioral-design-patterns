07cf3566c54265c9e4a010c97ba5a636370258ad
olya_nick <olya17nikolbas@gmail.com>
2024-09-12 00:20:32 +0300

    Добавлены классы и интерфейсы описывающие субъект и наблюдателей.

--- WeatherStation(Observer_pattern)/Program.cs
+++ WeatherStation(Observer_pattern)/Program.cs
@@ -1,4 +1,6 @@
-﻿namespace WeatherStation_Observer_pattern_
+﻿using System.Collections;
+
+namespace WeatherStation_Observer_pattern_
 {
     internal class Program
     {
@@ -6,5 +8,66 @@
         {
             Console.WriteLine("Hello, World!");
         }
+    }
+
+    public class WeatherData : ISubject
+    {
+        private ArrayList observers;
+        private float temperature;
+        private float humidity;
+        private float pressure;
+
+        public WeatherData()
+        {
+            observers = new ArrayList();
+        }
+
+        public void NotifyObservers()
+        {
+            foreach (IObserver observer in observers)
+                observer.Update(temperature, humidity, pressure);
+        }
+
+        public void RegisterObserver(IObserver o)
+        {
+            observers.Add(o);
+        }
+
+        public void RemoveObserver(IObserver o)
+        {
+            observers.Remove(o);
+        }
+
+        // Оповещение наблюдателей о появлении новых данных.
+        public void MeasurementsChanged()
+        {
+            NotifyObservers();
+        }
+
+        public void SetMeasurements(float temperature, float humidity, float pressure)
+        {
+            this.temperature = temperature;
+            this.humidity = humidity;
+            this.pressure = pressure;
+            MeasurementsChanged();
+        }
+    }
+
+    public interface ISubject
+    {
+        public void RegisterObserver(IObserver o);
+        public void RemoveObserver(IObserver o);
+        public void NotifyObservers();
+    }
+
+    public interface IObserver
+    {
+        // Данные передаваемые наблюдателям при изменении состояния субъекта.
+        public void Update(float temp, float humidity, float pressure);
+    }
+
+    public interface IDisplayElement
+    {
+        public void Display();
     }
 }
