71256d5a4431f4c3d6aeb83d3697d6bc89d0ad15
olya_nick <olya17nikolbas@gmail.com>
2024-09-12 00:09:40 +0300

    Добавлен проект реализуещий паттерн Наблюдатель

---
+++ WeatherStation(Observer_pattern)/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace WeatherStation_Observer_pattern_
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
