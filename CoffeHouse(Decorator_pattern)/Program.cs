b90be2d3b7ddf934c4ea80c845316b7c97ce2227
olya_nick <olya17nikolbas@gmail.com>
2024-09-17 00:20:30 +0300

    Создан проект CoffeHouse для демонстрации паттерна Декоратор

---
+++ CoffeHouse(Decorator_pattern)/Program.cs
@@ -0,0 +1,40 @@
+﻿using System.ComponentModel;
+
+namespace CoffeHouse_Decorator_pattern_
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+    public abstract class Beverage 
+    {
+        public string Description = "Unknow beverage";
+
+        public string GetDscription()
+        { 
+            return Description; 
+        }
+        public abstract double Cost();
+    }
+
+    public abstract class CondimentDecorator : Beverage
+    {
+        Beverage? beverage;
+        public abstract string GetDescription();
+    }
+
+    public class Espresso : Beverage
+    {
+        public Espresso()
+        {
+            Description = "Espresso";
+        }
+        public override double Cost()
+        {
+            return 1.99;
+        }
+    }
+}
