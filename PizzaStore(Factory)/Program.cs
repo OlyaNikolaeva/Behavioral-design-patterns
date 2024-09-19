47fda700579b866736245bd54e6fb6cc1cd704d8
olya_nick <olya17nikolbas@gmail.com>
2024-09-19 22:46:53 +0300

    Добавление фабрики

--- PizzaStore(Factory)/Program.cs
+++ PizzaStore(Factory)/Program.cs
@@ -9,6 +9,7 @@ namespace PizzaStore_Factory_
         {
             Console.WriteLine("Hello, World!");
         }
+
     }
 
     public abstract class Pizza
@@ -54,6 +55,66 @@ namespace PizzaStore_Factory_
                 display.Append(topping + "\n");
             }
             return display.ToString();
+        }
+    }
+
+    public class PizzaStore
+    {
+        public Pizza OrderPizza(string type)
+        {
+            Pizza pizza = null;
+
+            pizza.Prepare();
+            pizza.Bake();
+            pizza.Cut();
+            pizza.Box();
+            return pizza;
+        }
+    }
+
+    public class CheesePizza: Pizza
+    {
+        
+    }
+
+    public class GreekPizza : Pizza
+    {
+
+    }
+
+    public class PepperoniPizza : Pizza
+    {
+
+    }
+
+    public class ClamiPizza : Pizza
+    {
+
+    }
+
+    public class VeggiePizza : Pizza
+    {
+
+    }
+
+    public class SimplePizzaFactory
+    {
+        public Pizza CreatePizza(string type)
+        {
+            Pizza pizza = null;
+
+            if (type.Equals("cheese"))
+                pizza = new CheesePizza();
+            else if (type.Equals("greek"))
+                pizza = new GreekPizza();
+            else if (type.Equals("pepperoni"))
+                pizza = new PepperoniPizza();
+            else if (type.Equals("clam"))
+                pizza = new ClamiPizza();
+            else if (type.Equals("veggie"))
+                pizza = new VeggiePizza();
+
+            return pizza;
         }
     }
 }
