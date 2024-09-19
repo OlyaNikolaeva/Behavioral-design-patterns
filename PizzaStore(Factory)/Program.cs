02b0146f1da21de4971eff3945616031ea9e580c
olya_nick <olya17nikolbas@gmail.com>
2024-09-19 21:48:55 +0300

    Добавлен абстрактный класс Pizza

--- PizzaStore(Factory)/Program.cs
+++ PizzaStore(Factory)/Program.cs
@@ -1,10 +1,59 @@
-﻿namespace PizzaStore_Factory_
+﻿using System.Text;
+using System.Xml.Linq;
+
+namespace PizzaStore_Factory_
 {
     internal class Program
     {
         static void Main(string[] args)
         {
             Console.WriteLine("Hello, World!");
+        }
+    }
+
+    public abstract class Pizza
+    {
+        string? Name;
+        string? Dough;
+        string? Sauce;
+        List<string> Toppings = new List<string>();
+
+        public string? GetName()
+        { 
+            return Name; 
+        }
+
+        public void Prepare()
+        {
+            Console.WriteLine("Preparing " + Name);
+        }
+
+        public void Bake()
+        {
+            Console.WriteLine("Baking " + Name);
+        }
+
+        public void Cut()
+        {
+            Console.WriteLine("Cutting " + Name);
+        }
+
+        public void Box()
+        {
+            Console.WriteLine("Boxing " + Name);
+        }
+
+        public string Display()
+        {
+            StringBuilder display = new StringBuilder();
+            display.Append("---- " + Name + " ----\n");
+            display.Append(Dough + "\n");
+            display.Append(Sauce + "\n");
+            foreach (string topping in Toppings)
+            {
+                display.Append(topping + "\n");
+            }
+            return display.ToString();
         }
     }
 }
