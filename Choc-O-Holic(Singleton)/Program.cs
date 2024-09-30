9de50b23f90dd77d45beca79beb7271e50715a34
olya_nick <olya17nikolbas@gmail.com>
2024-09-30 22:14:13 +0300

    Create project for singletone pattern

---
+++ Choc-O-Holic(Singleton)/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace Choc_O_Holic_Singleton_
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
